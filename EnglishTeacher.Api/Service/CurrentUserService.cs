bb39038d64dc4f39f9c6d034c33d3cd0a82dfcc5
SebastianUryga <sebastian.uryga@gmail.com>
2022-09-30 17:25:43 +0200

    IndentityServer added AuditableEntity updated JwtBearer added

---
+++ EnglishTeacher.Api/Service/CurrentUserService.cs
@@ -0,0 +1,20 @@
+﻿using EnglishTeacher.Application.Common.Interfaces;
+using IdentityModel;
+using System.Security.Claims;
+
+namespace EnglishTeacher.Api.Service
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            var email = httpContextAccessor.HttpContext?.User?.FindFirstValue(JwtClaimTypes.Email);
+
+            Email = email;
+
+            IsAuthenticated = !string.IsNullOrEmpty(email);
+        }
+    }
+}
---
+++ IdentityServer/Services/ProfileService.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer.Models;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private UserManager<ApplicationUser> _userMenager;
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var user = await _userMenager.GetUserAsync(context.Subject);
+
+            var claims = new List<Claim>
+            {
+                new Claim("Email", user.Email)
+            };
+
+            context.IssuedClaims.AddRange(claims);
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var user = await _userMenager.GetUserAsync(context.Subject);
+            context.IsActive = user != null;
+        }
+    }
+}
